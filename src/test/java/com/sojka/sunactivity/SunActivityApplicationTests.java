45ee552de9d47133d4ef8829ed2e813adfc3aa5a
Mateusz Sójka <MateuszSojka@proton.me>
2023-06-05 21:46:57 +0200

    Initial commit.

---
+++ src/test/java/com/sojka/sunactivity/SunActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sojka.sunactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
