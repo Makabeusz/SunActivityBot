b4aea16968909b5754f7c781289704c61d1c9912
Mateusz Sójka <msojka@griddynamics.com>
2023-06-08 17:21:08 +0200

    Added Firestore to classpath and created config class

---
+++ src/main/java/com/sojka/sunactivity/config/FirebaseConfig.java
@@ -0,0 +1,25 @@
+package com.sojka.sunactivity.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    Firestore firestore(@Value("${cloud.google.project_id}") String projectId) throws IOException {
+        FirestoreOptions firestoreOptions =
+                FirestoreOptions.getDefaultInstance().toBuilder()
+                        .setProjectId(projectId)
+                        .setCredentials(GoogleCredentials.getApplicationDefault())
+                        .build();
+        return firestoreOptions.getService();
+    }
+
+}
