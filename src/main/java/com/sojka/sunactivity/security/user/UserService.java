93b56278e9fd16f3fb3de4882caf182f97d77124
Mateusz Sójka <msojka@griddynamics.com>
2023-07-06 12:28:33 +0200

    Implemented JwtAuthenticationFilter and UserDetailsService

---
+++ src/main/java/com/sojka/sunactivity/security/user/UserService.java
@@ -0,0 +1,22 @@
+package com.sojka.sunactivity.security.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = repository.findByEmail(username);
+        return user.orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
