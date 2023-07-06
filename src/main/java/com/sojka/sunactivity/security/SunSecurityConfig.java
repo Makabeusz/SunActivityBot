938417e910449c493391fcc33919b8084bae41b7
Mateusz Sójka <msojka@griddynamics.com>
2023-07-06 13:11:28 +0200

    Implemented SunSecurityConfig with one bean SecurityFilterChain

---
+++ src/main/java/com/sojka/sunactivity/security/SunSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.sojka.sunactivity.security;
+
+import com.sojka.sunactivity.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SunSecurityConfig {
+
+    private final JwtAuthenticationFilter authFilter;
+    private final AuthenticationProvider authProvider;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(r -> r
+                        .requestMatchers("/login")
+                        .permitAll())
+                .authorizeHttpRequests(r -> r
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+}
