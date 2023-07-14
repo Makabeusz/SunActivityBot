fb35836226061ef08934e9864d9860f12fcf115c
Mateusz Sójka <msojka@griddynamics.com>
2023-07-14 13:51:25 +0200

    Implemented DonkiHttpClient#getFlares

--- src/main/java/com/sojka/sunactivity/donki/http/DonkiHttpClient.java
+++ src/main/java/com/sojka/sunactivity/donki/http/DonkiHttpClient.java
@@ -1,6 +1,7 @@
 package com.sojka.sunactivity.donki.http;
 
 import com.sojka.sunactivity.donki.domain.Cme;
+import com.sojka.sunactivity.donki.domain.Flare;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,31 +28,37 @@ public class DonkiHttpClient {
 
     private static final String API_KEY = System.getenv("SUN_DONKI_KEY");
 
-    private final String cmeUrl;
+    private final String cmeUri;
+    private final String flareUri;
     private final RestTemplate rest;
 
     public DonkiHttpClient(@Value("${nasa.donki.api}") String apiUrl,
                            RestTemplate restTemplate) {
-        this.cmeUrl = apiUrl + "/CME";
+        this.cmeUri = apiUrl + "/CME";
+        this.flareUri = apiUrl + "/FLR";
         this.rest = restTemplate;
     }
 
     public ResponseEntity<Set<Cme>> getCMEs(LocalDate startDate, LocalDate endDate) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("api_key", API_KEY);
-        params.put("startDate", startDate);
-        params.put("endDate", endDate);
-
-        var uri = UriComponentsBuilder.fromHttpUrl(cmeUrl)
-                .queryParam("api_key", "{api_key}")
-                .queryParam("startDate", "{startDate}")
-                .queryParam("endDate", "{endDate}")
-                .build(params);
+        var uri = prepareUri(cmeUri, startDate, endDate);
+        log.info("Looking for CMEs in DONKI between {} and {}", startDate, endDate);
         try {
-            log.info("Looking for CMEs in DONKI between {} and {}", startDate, endDate);
             Cme[] cmes = rest.getForObject(uri, Cme[].class);
             Objects.requireNonNull(cmes);
             return ResponseEntity.ok(Arrays.stream(cmes).collect(Collectors.toSet()));
+        } catch (RestClientException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.ok(Collections.emptySet());
+        }
+    }
+
+    public ResponseEntity<Set<Flare>> getFlares(LocalDate startDate, LocalDate endDate) {
+        var uri = prepareUri(flareUri, startDate, endDate);
+        log.info("Looking for Solar Flares in DONKI between {} and {}", startDate, endDate);
+        try {
+            Flare[] flares = rest.getForObject(uri, Flare[].class);
+            Objects.requireNonNull(flares);
+            return ResponseEntity.ok(Arrays.stream(flares).collect(Collectors.toSet()));
         } catch (RestClientException e) {
             log.error(e.getMessage());
             return ResponseEntity.ok(Collections.emptySet());
@@ -66,8 +74,21 @@ public class DonkiHttpClient {
             return ResponseEntity.of(
                             ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), e.getMessage()))
                     .build();
-            // TODO: looks smelly ^
+            //  TODO: looks smelly ^
         }
+    }
+
+    private URI prepareUri(String uri, LocalDate startDate, LocalDate endDate) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("api_key", API_KEY);
+        params.put("startDate", startDate);
+        params.put("endDate", endDate);
+
+        return UriComponentsBuilder.fromHttpUrl(uri)
+                .queryParam("api_key", "{api_key}")
+                .queryParam("startDate", "{startDate}")
+                .queryParam("endDate", "{endDate}")
+                .build(params);
     }
 
     /**
